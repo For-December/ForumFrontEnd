6e16a5882c89f464be6caf0780800c94f4091473
forDecember <1921567337@qq.com>
2023-11-04 00:34:34 +0800

    update: 将登录页面从Auth分离

--- src/pages/Auth.vue
+++ src/pages/Auth.vue
@@ -3,71 +3,9 @@
     <van-tabs v-model:active="activeName">
       <van-tab title="登录" name="Login">
 
-        <div style="margin-left: 5%;margin-right: 5%">
-          <p></p>
-          <van-row justify="center">
-            <van-col span="6"></van-col>
-            <van-col span="6" offset="2">
-              <van-image
-                  style="margin: 0 auto"
-                  round
-                  width="6rem"
-                  height="6rem"
-                  src="https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg"
-              />
-            </van-col>
-            <van-col span="6"></van-col>
-          </van-row>
+<!--        实现LoginPage定义的接口-->
+        <LoginPage @loginSuccess="loginSuccess"/>
 
-
-          <p></p>
-          <!-- 输入任意文本 -->
-          <!--          <van-field v-model="username" label="用户名"/>-->
-          <div>
-            <el-form :model="loginForm" :rules="rules" ref="loginFormRef">
-              <el-form-item size="large" prop="username">
-                <el-input v-model="loginForm.username" maxlength="25" type="text" placeholder="用户名/邮箱">
-                  <template #prefix>
-                    <el-icon>
-                      <User/>
-                    </el-icon>
-                  </template>
-                </el-input>
-              </el-form-item>
-
-              <el-form-item size="large" prop="password">
-                <el-input v-model="loginForm.password" maxlength="25" type="password" placeholder="密码">
-                  <template #prefix>
-                    <el-icon>
-                      <Lock/>
-                    </el-icon>
-                  </template>
-                </el-input>
-              </el-form-item>
-            </el-form>
-          </div>
-          <!-- 输入手机号，调起手机号键盘 -->
-          <!--        <van-field v-model="tel" type="tel" label="手机号" />-->
-          <!-- 允许输入正整数，调起纯数字键盘 -->
-          <!--        <van-field v-model="digit" type="digit" label="整数" />-->
-          <!-- 允许输入数字，调起带符号的纯数字键盘 -->
-          <!--        <van-field v-model="number" type="number" label="数字" />-->
-          <!-- 输入密码 -->
-          <!--          <van-field v-model="password" type="password" label="密码"/>-->
-          <el-row>
-            <el-col :span="12" style="text-align: left">
-              <el-form-item prop="remember">
-                <el-checkbox v-model="loginForm.remember" label="记住我"/>
-              </el-form-item>
-            </el-col>
-            <el-col :span="12" style="text-align: right">
-              <el-link type="success">忘记密码？</el-link>
-            </el-col>
-          </el-row>
-          <div>
-            <van-button round type="success" size="large" @click="userLogin">登录</van-button>
-          </div>
-        </div>
       </van-tab>
       <van-tab title="注册喵" name="Register">
 
@@ -194,8 +132,8 @@ import {ref, reactive} from "vue";
 import {ElMessage} from 'element-plus'
 import {login} from "@/api/auth.ts";
 import {storeAuthInfo} from "@/plugins/myAxios.ts";
-
-const emit = defineEmits(['loginSuccess'])
+import {authed} from "@/plugins/globalData.ts";
+import LoginPage from "@/pages/auths/LoginPage.vue";
 
 
 
@@ -229,6 +167,16 @@ defineExpose({
 
 // login
 
+// 定义接口保留给 Home.vue
+const emit = defineEmits(['loginSuccess'])
+const loginSuccess = () => {
+  // 调用 Home 定义的方法，给 Home.vue 传信息
+  emit('loginSuccess')
+  isShow.value = false;
+  authed.value = true;
+}
+
+
 const rules = {
   username: [
     {required: true, message: '请输入用户名'}
@@ -237,33 +185,6 @@ const rules = {
     {required: true, message: '请输入密码'}
   ]
 }
-
-const loginForm = reactive({
-  username: "",
-  password: "",
-  remember: false
-});
-const userLogin = () => {
-  loginFormRef.value.validate((valid) => {
-    if (valid) {
-      login(loginForm).then((data) => {
-        console.log(data);
-        isShow.value = false;
-        emit('loginSuccess');
-        ElMessage.success("登陆成功！")
-        storeAuthInfo(data)
-      }).catch((err) => {
-        console.log(err)
-        console.log("出错了")
-      });
-
-
-      // ElMessage.success("哈哈哈哈哈！！！！！！")
-    }
-  })
-}
-const loginFormRef = ref();
-
 
 </script>
 
---
+++ src/pages/auths/LoginPage.vue
@@ -0,0 +1,122 @@
+<script setup lang="ts">
+
+import {reactive, ref} from "vue";
+import {login} from "@/api/auth.ts";
+import {ElMessage} from "element-plus";
+import {storeAuthInfo} from "@/plugins/myAxios.ts";
+import {Lock, User} from "@element-plus/icons-vue";
+
+const rules = {
+  username: [
+    {required: true, message: '请输入用户名'}
+  ],
+  password: [
+    {required: true, message: '请输入密码'}
+  ]
+}
+
+// 为Auth定义接口
+const emit = defineEmits(['loginSuccess'])
+const loginForm = reactive({
+  username: "",
+  password: "",
+  remember: false
+});
+const userLogin = () => {
+  loginFormRef.value.validate((valid: Boolean) => {
+    if (valid) { // 格式校验合法
+      login(loginForm).then((data) => {
+        console.log(data);
+
+        // 调用Auth中的函数来传递信息
+        emit('loginSuccess');
+        // isShow.value = false; 该语句通过上句在Auth执行
+
+        ElMessage.success("登陆成功！")
+        storeAuthInfo(data)
+      }).catch((err) => {
+        console.log(err)
+        console.log("出错了")
+      });
+
+
+      // ElMessage.success("哈哈哈哈哈！！！！！！")
+    }
+  })
+}
+const loginFormRef = ref();
+
+</script>
+
+<template>
+  <div style="margin-left: 5%;margin-right: 5%">
+    <p></p>
+    <van-row justify="center">
+      <van-col span="6"></van-col>
+      <van-col span="6" offset="2">
+        <van-image
+            style="margin: 0 auto"
+            round
+            width="6rem"
+            height="6rem"
+            src="https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg"
+        />
+      </van-col>
+      <van-col span="6"></van-col>
+    </van-row>
+
+
+    <p></p>
+    <!-- 输入任意文本 -->
+    <!--          <van-field v-model="username" label="用户名"/>-->
+    <div>
+      <el-form :model="loginForm" :rules="rules" ref="loginFormRef">
+        <el-form-item size="large" prop="username">
+          <el-input v-model="loginForm.username" maxlength="25" type="text" placeholder="用户名/邮箱">
+            <template #prefix>
+              <el-icon>
+                <User/>
+              </el-icon>
+            </template>
+          </el-input>
+        </el-form-item>
+
+        <el-form-item size="large" prop="password">
+          <el-input v-model="loginForm.password" maxlength="25" type="password" placeholder="密码">
+            <template #prefix>
+              <el-icon>
+                <Lock/>
+              </el-icon>
+            </template>
+          </el-input>
+        </el-form-item>
+      </el-form>
+    </div>
+    <!-- 输入手机号，调起手机号键盘 -->
+    <!--        <van-field v-model="tel" type="tel" label="手机号" />-->
+    <!-- 允许输入正整数，调起纯数字键盘 -->
+    <!--        <van-field v-model="digit" type="digit" label="整数" />-->
+    <!-- 允许输入数字，调起带符号的纯数字键盘 -->
+    <!--        <van-field v-model="number" type="number" label="数字" />-->
+    <!-- 输入密码 -->
+    <!--          <van-field v-model="password" type="password" label="密码"/>-->
+    <el-row>
+      <el-col :span="12" style="text-align: left">
+        <el-form-item prop="remember">
+          <el-checkbox v-model="loginForm.remember" label="记住我"/>
+        </el-form-item>
+      </el-col>
+      <el-col :span="12" style="text-align: right">
+        <el-link type="success">忘记密码？</el-link>
+      </el-col>
+    </el-row>
+    <div>
+      <van-button round type="success" size="large" @click="userLogin">登录</van-button>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
