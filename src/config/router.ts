a13427a8ede8ad3926d424c047a2e0082fea67da
forDecember <1921567337@qq.com>
2023-11-11 16:16:36 +0800

    update: 将 router所有的配置逻辑转移至 router.ts

--- src/config/router.ts
+++ src/config/router.ts
@@ -1,3 +1,7 @@
+import {createRouter, createWebHistory} from "vue-router";
+import {authed} from "@/plugins/globalData.ts";
+import {authAndLogin} from "@/plugins/globalFunc.ts";
+
 const routes = [
     {
         path: '/home',
@@ -8,19 +12,19 @@ const routes = [
     {
         path: '/tree-hole',
         name: 'tree-hole',
-        meta:{title: "树洞"},
+        meta: {title: "树洞"},
         component: () => import('@/pages/TreeHole.vue')
     },
     {
         path: '/profile',
         name: "profile",
-        meta:{title: "个人主页"},
+        meta: {title: "个人主页"},
         component: () => import('@/pages/Profile.vue')
     },
     {
         path: '/settings',
         name: 'settings',
-        meta: {title:"设置"},
+        meta: {title: "设置"},
         component: () => import('@/pages/Settings.vue')
     },
     {
@@ -31,4 +35,47 @@ const routes = [
     },
 
 ]
-export default routes;
+
+// 3. 创建路由实例并传递 `routes` 配置
+// 你可以在这里输入更多的配置，但我们在这里
+// 暂时保持简单
+const router = createRouter({
+    // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
+    history: createWebHistory(),
+    routes, // `routes: routes` 的缩写
+})
+
+router.beforeEach((to, from, next) => {
+
+    console.log(from.path)
+    console.log(to.path)
+    // 检查用户是否已登录
+    if (
+        !authed.value &&
+        // ❗️ 避免无限重定向
+        to.name !== 'home'
+        || from.path === '/' // 访问根目录
+        && to.name !== 'home'
+    ) {
+        authAndLogin().then((isLogin) => {
+            if (isLogin) {
+                console.log('已登录')
+                // 不重定向
+                next()
+                return
+            }
+            console.log(isLogin)
+            console.log("未登录~")
+            // 将用户重定向到登录页面
+            next({name: 'home'})
+        });
+
+
+    } else {
+
+        document.title = `${to.meta.title} | 测试`;
+        next();
+    }
+});
+
+export default router;
--- src/main.ts
+++ src/main.ts
@@ -2,66 +2,11 @@ import {createApp} from 'vue'
 import App from './App.vue'
 import {Button, NavBar, Popup} from "vant";
 
-// @ts-ignore
-import * as VueRouter from 'vue-router';
-import routes from "./config/router";
 import 'element-plus/dist/index.css'
-import {authed, curUser, curUserId} from "@/plugins/globalData.ts";
-import {takeAuthObj} from "@/plugins/myAxios.ts";
-import {userInfo} from "@/api/auth.ts";
-import {authAndLogin} from "@/plugins/globalFunc.ts";
+import router from "./config/router";
 
 
-// 3. 创建路由实例并传递 `routes` 配置
-// 你可以在这里输入更多的配置，但我们在这里
-// 暂时保持简单
-const router = VueRouter.createRouter({
-    // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
-    history: VueRouter.createWebHistory(),
-    routes, // `routes: routes` 的缩写
-})
-router.beforeEach((to, from, next) => {
 
-    console.log(from.path)
-    console.log(to.path)
-    // 检查用户是否已登录
-    if (
-        !authed.value &&
-        // ❗️ 避免无限重定向
-        to.name !== 'home'
-        || from.path === '/' // 访问根目录
-        && to.name !== 'home'
-    ) {
-        authAndLogin().then((isLogin) => {
-            if (isLogin) {
-                console.log('已登录')
-                // 不重定向
-                next()
-                return
-            }
-            console.log(isLogin)
-            console.log("未登录~")
-            // 将用户重定向到登录页面
-            next({name: 'home'})
-        });
-
-
-    } else {
-
-        document.title = `${to.meta.title} | 测试`;
-        next();
-    }
-});
-
-
-// router.beforeEach((to, from) => {
-//     // ...
-//     // 返回 false 以取消导航
-//
-//     console.log(from.path)
-//     console.log(to.path)
-//     return false
-// })
 
 createApp(App)
     .use(Button)
