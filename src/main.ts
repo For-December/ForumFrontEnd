1dff0e4bd3c6bb99003b219f621c18db0f375f22
forDecember <1921567337@qq.com>
2023-11-11 15:11:52 +0800

    update: 添加路由守卫

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import {Button, NavBar, Popup} from "vant";
 import * as VueRouter from 'vue-router';
 import routes from "./config/router";
 import 'element-plus/dist/index.css'
+import {authed} from "@/plugins/globalData.ts";
 
 
 // 3. 创建路由实例并传递 `routes` 配置
@@ -13,14 +14,40 @@ import 'element-plus/dist/index.css'
 // 暂时保持简单
 const router = VueRouter.createRouter({
     // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
-    history: VueRouter. createWebHistory(),
+    history: VueRouter.createWebHistory(),
     routes, // `routes: routes` 的缩写
 })
 router.beforeEach((to, from, next) => {
-    document.title = `${to.meta.title} | 泡泡 - 一个清新文艺的微社区`;
-    next();
+
+    console.log(from.path)
+    console.log(to.path)
+    // 检查用户是否已登录
+    if (
+        !authed.value &&
+        // ❗️ 避免无限重定向
+        to.name !== 'home'
+        || from.path === '/' // 访问根目录
+        && to.name !== 'home'
+    ) {
+        console.log("未登录~")
+        // 将用户重定向到登录页面
+        next({name: 'home'})
+    } else {
+
+        document.title = `${to.meta.title} | 泡泡 - 一个清新文艺的微社区`;
+        next();
+    }
 });
 
+
+// router.beforeEach((to, from) => {
+//     // ...
+//     // 返回 false 以取消导航
+//
+//     console.log(from.path)
+//     console.log(to.path)
+//     return false
+// })
 
 createApp(App)
     .use(Button)
